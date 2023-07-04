c2a8ec53539c5643e5c4f5322951961106778fae
DarthSalad <piyushmishra965@gmail.com>
2023-07-04 18:16:42 +0530

    cors config updated in app.py

--- server/app.py
+++ server/app.py
@@ -10,72 +10,84 @@ load_dotenv()
 key = os.environ.get("YT_DATA_API")
 
 app = Flask(__name__)
-CORS(app)
-app.config['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-app.config['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
-app.config['Access-Control-Allow-Credentials'] = 'true'
-app.config['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+CORS(
+    app,
+    origins=["http://localhost:3000"],
+    supports_credentials=True,
+    allow_headers=["Content-Type", "Authorization"],
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+)
+# app.config["Access-Control-Allow-Origin"] = "http://localhost:3000"
+# app.config[
+#     "Access-Control-Allow-Headers"
+# ] = "Content-Type, Authorization, X-Requested-With"
+# app.config["Access-Control-Allow-Credentials"] = "true"
+# app.config["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+
 
 # dynamic video route with video id
-@app.route('/song', methods=['GET'])
+@app.route("/song", methods=["GET"])
 def get_song_details():
-  song_name = request.args.get("name")
-  artist = request.args.get("artist")
-  url = "https://www.googleapis.com/youtube/v3/search"
-  response = requests.get(
-      url,
-      params={
-          "part": "snippet",
-          "q": f"{song_name} {artist} Official Audio",
-          "key": key,
-          "type": "video",
-          "maxResults": 1,
-      },
-  )
+    song_name = request.args.get("name")
+    artist = request.args.get("artist")
+    url = "https://www.googleapis.com/youtube/v3/search"
+    response = requests.get(
+        url,
+        params={
+            "part": "snippet",
+            "q": f"{song_name} {artist} Official Audio",
+            "key": key,
+            "type": "video",
+            "maxResults": 1,
+        },
+    )
 
-  json_data=response.json()
-  video_id=json_data['items'][0]['id']['videoId']
+    json_data = response.json()
+    video_id = json_data["items"][0]["id"]["videoId"]
 
-  class MyLogger(object):
-      def debug(self, msg):
-          pass
+    class MyLogger(object):
+        def debug(self, msg):
+            pass
 
-      def warning(self, msg):
-          pass
+        def warning(self, msg):
+            pass
 
-      def error(self, msg):
-          print(msg)
+        def error(self, msg):
+            print(msg)
+
+    def my_hook(d):
+        if d["status"] == "finished":
+            print("Done downloading, now converting ...")
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": "%(title)s.%(ext)s",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
+        "logger": MyLogger(),
+        "progress_hooks": [my_hook],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([f"https://www.youtube.com/watch?v={video_id}"])
+
+    for file in os.listdir():
+        if file.endswith(".mp3"):
+            return send_file(file, as_attachment=True)
 
 
-  def my_hook(d):
-      if d['status'] == 'finished':
-          print('Done downloading, now converting ...')
-
-  ydl_opts = {
-      'format': 'bestaudio/best',
-      'outtmpl': '%(title)s.%(ext)s',
-      'postprocessors': [{
-          'key': 'FFmpegExtractAudio',
-          'preferredcodec': 'mp3',
-          'preferredquality': '192',
-      }],
-      'logger': MyLogger(),
-      'progress_hooks': [my_hook],
-  }
-
-  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-      ydl.download([f"https://www.youtube.com/watch?v={video_id}"])
-      
-  for file in os.listdir():
-   if file.endswith(".mp3"):
-       return send_file(file, as_attachment=True)
-   
-@app.route("/cleanup", methods=['GET'])
+@app.route("/cleanup", methods=["GET"])
 def cleanup():
     for file in os.listdir():
-     if file.endswith(".mp3"):
-        os.remove(file)
-    
-    return jsonify({"message": "Cleanup successful"}), 200 
+        if file.endswith(".mp3"):
+            os.remove(file)
+
+    return jsonify({"message": "Cleanup successful"}), 200
+
 
 app.run(debug=True, port=8080)
